dbfef24cf74288d0772a1b18717c32337d1ec132
OG-RipBee <grantiv18@outlook.com>
2025-03-05 10:36:46 -0800

    Add index.js to frontend directory

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ShiftPlanner from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ShiftPlanner />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
